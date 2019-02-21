b138c1cbb120cc2c0f82c2ff2378f04b4b6247fe
unknown <bobqian1007@outlook.com>
2019-02-21 02:56:25 -0500

    unfinish, need add avoid method

---
+++ Lab5/src/ca/mcgill/ecse211/lab5/Lab5.java
@@ -0,0 +1,136 @@
+package ca.mcgill.ecse211.lab5;
+import lejos.hardware.Button;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.Port;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.SampleProvider;
+import ca.mcgill.ecse211.lab5.LightLocalizer;
+import ca.mcgill.ecse211.lab5.Display;
+import ca.mcgill.ecse211.lab5.Odometer;
+import ca.mcgill.ecse211.lab5.OdometerExceptions;
+import ca.mcgill.ecse211.lab5.UltrasonicLocalizer;
+
+public class Lab5 {
+  // Motor Objects, and Robot related parameters
+  private static final EV3LargeRegulatedMotor leftMotor =
+  new EV3LargeRegulatedMotor(LocalEV3.get().getPort("A"));
+  private static final EV3LargeRegulatedMotor rightMotor =
+  new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+  private static final Port usPort = LocalEV3.get().getPort("S1"); //port for the Ultrasonic Sensor
+  private static final TextLCD lcd = LocalEV3.get().getTextLCD();
+  public static final double WHEEL_RAD = 2.10;
+  public static final double TRACK = 13.45 ;
+  public static final double tileSize = 30.48;
+  
+  public static final int[] LL = {3,3};
+  public static final int[] UR = {6,6};
+  public static final int SC  = 0;
+  public static final String[] colors = {"red","green","blue","yellow"};
+  public static int targetColor = 0;
+  
+  //colorDetector sensor related objects
+  static EV3ColorSensor colorSensor = new EV3ColorSensor(LocalEV3.get().getPort("S3"));
+   
+  // Ultrasonic sensor related objects
+  private static SampleProvider sampleProviderUS;
+  static EV3UltrasonicSensor usSensor;
+  static float[] usValues;
+	  
+  LightLocalizer ll;
+	  
+  public static Odometer odometer;
+  public static void main(String[] args) throws InterruptedException,OdometerExceptions {
+		// TODO Auto-generated method stub
+	int buttonChoice;
+	do {
+	  // clear the display
+	  lcd.clear();
+
+	  // ask the user use color detection or field test
+	  lcd.drawString("< Left | Right >", 0, 0);
+	  lcd.drawString("       |        ", 0, 1);
+	  lcd.drawString("color  | field  ", 0, 2);
+	  lcd.drawString("detect | test   ", 0, 3);
+	  lcd.drawString("       |        ", 0, 4);
+
+	  buttonChoice = Button.waitForAnyPress(); // Record choice (left or right press)
+	} while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT);
+	//choose the condition that use color detection or field test
+    if (buttonChoice == Button.ID_LEFT) { 
+      int color;
+      lcd.clear();
+      ColorClassification colorDetector = new ColorClassification(colorSensor);
+      usSensor = new EV3UltrasonicSensor(usPort);
+      sampleProviderUS = usSensor.getMode("Distance");
+      usValues = new float[sampleProviderUS.sampleSize()];
+      for (int i = 0 ; i < 5;i++) {
+    	sampleProviderUS.fetchSample(usValues, 0);
+    	while(usValues[0] > 10) {
+    	  sampleProviderUS.fetchSample(usValues, 0);
+    	}
+    	  color = colorDetector.findColor();
+    	  lcd.drawString("object detected", 0, 1);
+    	  lcd.drawString(colors[color], 0, 2);
+    	  if (Button.waitForAnyPress() == Button.ID_ESCAPE){
+    	    System.exit(0);
+    	  }
+    	  else {
+    		Thread.sleep(1000);  
+    	  }
+      }
+      
+    } else if (buttonChoice == Button.ID_RIGHT) { //field
+      // Odometer related objects
+      Odometer odometer = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);//Completed implementation
+      Display odometryDisplay = new Display(lcd); // No need to change
+      
+      //setup ultrasonic sensor
+      usSensor = new EV3UltrasonicSensor(usPort);
+      sampleProviderUS = usSensor.getMode("Distance");
+      usValues = new float[sampleProviderUS.sampleSize()];
+  	  //construct  a UltrasonicLocalizer	
+      UltrasonicLocalizer usl = new UltrasonicLocalizer(sampleProviderUS, usValues, leftMotor, rightMotor, SC);
+      
+      //setup threads
+      Thread odoThread = new Thread(odometer);
+      odoThread.start();
+      Thread odoDisplayThread = new Thread(odometryDisplay);
+      odoDisplayThread.start();
+      
+      //Do localization with ultrasonic sensor    	
+      usl.doLocalization();
+      
+      //wait for press do see angle
+      Button.waitForAnyPress();
+      usSensor.close();
+      //set up the light 
+      LightLocalizer ll = new LightLocalizer( leftMotor, rightMotor,SC);
+      //Do localization with light sensor
+      ll.doLocalization();
+      usSensor = new EV3UltrasonicSensor(usPort);
+      sampleProviderUS = usSensor.getMode("Distance");
+      usValues = new float[sampleProviderUS.sampleSize()];
+      ColorClassification colorDetector = new ColorClassification(colorSensor);
+      Navigation navigation = new Navigation(leftMotor,rightMotor,targetColor,UR[0],LL[0],UR[1],LL[1],colorDetector);
+      SensorPoller usPoller = new SensorPoller(usSensor,usValues,leftMotor,rightMotor,navigation);
+     Thread usThread = new Thread(usPoller);
+     usThread.start();
+     Thread navigate = new Thread(navigation);
+     navigate.start();
+    		 
+    		 
+    		 
+      
+      
+      
+      while (Button.waitForAnyPress() != Button.ID_ESCAPE);
+      System.exit(0);
+    }
+  }
+
+}
---
+++ Lab5/src/ca/mcgill/ecse211/lab5/Navigation.java
@@ -0,0 +1,339 @@
+package ca.mcgill.ecse211.lab5;
+
+import ca.mcgill.ecse211.lab5.Odometer;
+import ca.mcgill.ecse211.lab5.OdometerExceptions;
+import lejos.hardware.Sound;
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.TextLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
+
+/**
+ * This class is used to drive the robot on the demo floor.
+ */
+public class Navigation extends Thread{
+  //private Odometer odometer;
+ 
+  //private Odometer odometer;
+  private static final int FORWARD_SPEED = 180;
+  public static final String[] colors = {"red","green","blue","yellow"};
+  private int targetColour;
+  private int LLx;
+  private int URx;
+  private int LLy;
+  private int URy;
+  public static final double tileSize = 30.48;
+  private static final EV3MediumRegulatedMotor sensorMotor =
+	      new EV3MediumRegulatedMotor(LocalEV3.get().getPort("C"));
+  private static final int ROTATE_SPEED = 120;
+  private static final double TILE_SIZE = 30.48;
+  public static final Odometer odometer = Lab5.odometer;
+  private static EV3LargeRegulatedMotor leftMotor ;
+  private static EV3LargeRegulatedMotor rightMotor;
+  private ColorClassification colorDetector;
+  static final double leftRadius = 2.11;
+  static final double rightRadius = 2.11;
+  static final double track = 13.8;
+  private static final int safeDistance= 10;
+  private static boolean travelling;
+  private boolean planning;
+  private boolean found;
+   static double jobx;
+   static double joby;
+   public boolean search;
+   static boolean avoid=false;
+  
+  
+  /**
+   * This is the default class constructor. An existing instance of the odometer is used. This is to
+   * ensure thread safety.
+   * 
+   * @throws OdometerExceptions
+   */
+  public Navigation(EV3LargeRegulatedMotor leftmotor,EV3LargeRegulatedMotor rightmotor,int targetColour,int URx,int LLx,int URy,int LLy,ColorClassification colorDetector) throws OdometerExceptions {
+	this.LLx = LLx;
+	this.LLy = LLy;
+	this.URx = URx;
+	this.URy = URy;
+	this.targetColour = targetColour;
+	this.found = false;
+	this.colorDetector = colorDetector;
+	this.search = false;
+	leftMotor = leftmotor;
+    rightMotor = rightmotor;
+    jobx = 0;
+    joby = 0;
+    
+  }
+
+  /**
+   * This method is meant to drive the robot in a square of size 2x2 Tiles. It is to run in parallel
+   * with the odometer and Odometer correcton classes allow testing their functionality.
+   * 
+   * @param leftMotor
+   * @param rightMotor
+   * @param leftRadius
+   * @param rightRadius
+   * @param width
+   */
+  public void travelTo(double x,double y ) throws OdometerExceptions{
+	jobx = x;
+	joby = y;
+	double theta, dX, dY, distance;
+    for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] {leftMotor, rightMotor}) {
+      motor.stop();
+      motor.setAcceleration(3000);
+    }
+
+    // Sleep for 2 seconds
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      // There is nothing to be done here
+    }
+
+    //TO DO SEE POSITION TO NEXT POSITION 
+   // angle = odometer.getXYT()[2];
+    
+    dX = x - odometer.getXYT()[0];
+    dY = y - odometer.getXYT()[1];
+    distance = Math.sqrt(dX*dX + dY*dY);
+    theta = Math.toDegrees(Math.atan(dX/dY));
+    
+    if(dX < 0 && dY < 0) {
+    	theta += 180;
+    }
+    if(dX > 0 && dY < 0) {
+    	theta += 180;
+    }
+    if(dX < 0 && dY > 0) {
+    	theta += 360;
+    }
+    turnTo(theta);
+    travelling = true;
+   
+    leftMotor.setSpeed(FORWARD_SPEED);
+    rightMotor.setSpeed(FORWARD_SPEED);
+
+    leftMotor.rotate(convertDistance(leftRadius, distance), true);
+    rightMotor.rotate(convertDistance(rightRadius, distance), true);
+    
+    travelling = false;
+   
+  }
+  
+  public  void avoid(double distance) throws OdometerExceptions{
+	int color = 4;
+    leftMotor.stop(true);
+	rightMotor.stop();
+	if(planning) {
+	  color = colorDetector.findColor();
+	  if (color == targetColour) {
+	    Sound.beep();
+	    this.found = true;
+	    return;
+	  }else {
+	    Sound.twoBeeps();
+	  }
+	}
+	travelling = false;
+	if (distance < safeDistance) {
+	  if (predictPath() == 1){
+	    RightAvoid(color);
+	  }
+	  else if (predictPath() == 0){
+	    leftAvoid(color);
+	  }
+	}
+	if (!planning) {
+		travelTo(jobx,joby);   
+	}
+	   
+  }
+  
+  
+
+  /**
+   * This method allows the conversion of a distance to the total rotation of each wheel need to
+   * cover that distance.
+   * 
+   * @param radius
+   * @param distance
+   * @return
+   */
+  public void turnTo (double theta) {
+	  double angle,smallestAngle;
+	  angle = odometer.getXYT()[2];
+	  
+	    if((theta - angle) > 180) {
+	    	smallestAngle = theta - angle - 360;
+	    }
+	    else if((theta - angle) < -180) {
+	    	smallestAngle = theta - angle + 360;
+	    }
+	    else {
+	    	smallestAngle = theta - angle;
+	    }
+	        // turn 90 degrees clockwise
+	        leftMotor.setSpeed(ROTATE_SPEED);
+	        rightMotor.setSpeed(ROTATE_SPEED);
+
+	        leftMotor.rotate(convertAngle(leftRadius, track, smallestAngle), true);
+	        rightMotor.rotate(-convertAngle(rightRadius, track, smallestAngle), false);
+  }
+  static boolean  isNavigating(){
+	  return travelling;
+  }
+  public int predictPath() {
+
+		double currx = odometer.getXYT()[0];
+		double curry = odometer.getXYT()[1];
+		double currTheta = odometer.getXYT()[2];
+		
+		if (currTheta > 350 || currTheta <= 10) {//going up
+			if (currx < (LLx+0.5)*tileSize -1) {
+				return 1;
+				//wallFollowRight();            // 1 represents right dodge and 0 represents left dodge
+			} 
+			else {
+				return 0;
+				//wallFollowLeft();
+			}
+		} 
+		else if(currTheta >= 80 && currTheta < 100){//going right
+			if (curry < (LLy+0.5)*tileSize) {
+				return 0;
+				//wallFollowLeft();
+			} 
+			else {
+				return 1;
+				//wallFollowRight();
+			}
+		}
+		else if(currTheta > 170 && currTheta < 190){//going down
+			if (currx < (URx-0.5)*tileSize) {
+				return 0;
+				//wallFollowLeft();
+			} 
+			else {
+				return 1;
+				//wallFollowRight();
+			}
+		}
+		else if(currTheta > 260 && currTheta < 280){//going left
+			if (curry <= (LLy+0.5)*tileSize ) {
+				return 1;
+				//wallFollowRight();
+			} 
+			else {
+				return 0;
+				//wallFollowLeft();
+			}
+		}
+		else { 
+			//wallFollowRight();
+			return 1;
+			}
+	}
+  public void RightAvoid(int color) {
+	  
+	}
+	
+	
+	public void leftAvoid(int color) {
+		
+	}
+	
+
+
+  private static int convertDistance(double radius, double distance) {
+    return (int) ((180.0 * distance) / (Math.PI * radius));
+  }
+
+  private static int convertAngle(double radius, double width, double angle) {
+    return convertDistance(radius, Math.PI * width * angle / 360.0);
+  }
+  public void verticalPlanning(int Rx,int Ry) throws OdometerExceptions {
+	  boolean firstSide = true;
+	  this.planning = true;
+	  for(int i = 1;i <= Ry;i++) {
+		    if(firstSide) {
+		      travelTo(this.LLx*tileSize,(this.LLy+1)*tileSize);
+		    }else {
+		    	travelTo(this.URx*tileSize,(this.LLy+1)*tileSize);
+		    }
+		  if(firstSide) {
+			  turnTo(90);
+		  }else {
+			  turnTo(270);
+		  }
+		  if (SensorPoller.usData[0]<(Rx*tileSize)) {
+			  if(firstSide) {
+				  travelTo(this.URx*tileSize,(this.LLy+1)*tileSize);
+			  }else {
+				  travelTo(this.LLx*tileSize,(this.LLy+1)*tileSize);
+			  }
+			  firstSide = false;
+		  }
+		  if (this.found) {
+			  break;
+		  }
+	  }
+	  travelTo(URx*tileSize,URy*tileSize);
+  }
+  public void horizontalPlanning(int Rx,int Ry) throws OdometerExceptions {
+	  boolean firstSide = true;
+	  this.planning = true;
+	  for(int i = 1;i <= Rx;i++) {
+		    if(firstSide) {
+		      travelTo((this.LLx+1)*tileSize,this.LLy*tileSize);
+		    }else {
+		    	travelTo((this.LLx+1)*tileSize,this.URy*tileSize);
+		    }
+		  if(firstSide) {
+			  turnTo(0);
+		  }else {
+			  turnTo(180);
+		  }
+		  if (SensorPoller.usData[0]<(Rx*tileSize)) {
+			  if(firstSide) {
+				  travelTo((this.LLx+1)*tileSize,this.URy*tileSize);
+			  }else {
+				  travelTo((this.LLx+1)*tileSize,this.LLy*tileSize);
+			  }
+			  firstSide = false;
+		  }
+		  if (this.found) {
+			  break;
+		  }
+	  }
+	  travelTo(URx*tileSize,URy*tileSize);
+  }
+  public void run() {
+	  try {
+		travelTo(LLx * tileSize,LLy*tileSize);
+	} catch (OdometerExceptions e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	  int Rx = this.URx - this.LLx;
+	  int Ry = this.URx - this.LLx;
+	  if (Rx <= Ry) {
+		  try {
+		  verticalPlanning(Rx,Ry);
+		  }catch (OdometerExceptions e) {
+				
+				e.printStackTrace();
+			}
+	  }
+	  else {
+		  try {
+		  horizontalPlanning(Rx,Ry);
+		  }catch (OdometerExceptions e) {
+				
+				e.printStackTrace();
+			}
+	  }
+  }
+}
